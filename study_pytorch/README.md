db9f97ccdb9cba1dde3f3c5c1a588b6c629b9d6b
YaGAO <1356551729@qq.com>
2021-01-16 13:13:54 +0800

    笔记

---
+++ study_pytorch/README.md
@@ -0,0 +1,20 @@
+
+《深度学习与PyTorch入门实战》
+
+在线视频：https://www.bilibili.com/video/BV1cV411Y7jZ?p=9
+网盘资源：https://pan.baidu.com/s/1VVZT3DeeQBHmdMtJW57Kwg    密码：r94a 
+
+
+
+笔记：
+ps:个人记录，代码手敲。网盘资源有视频、源码。
+
+tensorflow调试很麻烦，API调用困难。市场上用的多。
+
+1.pytorch常用API.pytorch库：可以GPU数据加速的模块集合。
+2.配置pytorchGPU版本。已安装cuda后，官网查找pytorch gpu版本下载命令https://pytorch.org/get-started/locally/      
+3.learing_rate一般设置为0.001，防止梯度下降的时候有较大波动。
+4.梯度下降有多种求解器可以提高计算速度。
+5.基本数据类型一一对应。
+pytorch处理string，可以使用one-hot编码表示。
+6.pytorch size()与shape区别。https://blog.csdn.net/weixin_42255190/article/details/112393897
--- study_pytorch/test.py
+++ study_pytorch/test.py
@@ -1,5 +1,6 @@
 import torch
 from torch import autograd
+import numpy as np
 
 x = torch.tensor(1.)
 a = torch.tensor(1., requires_grad=True)
@@ -11,3 +12,26 @@ y = a**2 * x + b * x + c
 print('before:', a.grad, b.grad, c.grad)
 grads = autograd.grad(y, [a, b, c])
 print('after:', grads[0], grads[1], grads[2])  #abc的偏微分
+
+torch.tensor(1.)
+z = torch.randn(2,3)  #在cpuTrue
+print(a.type())
+print(isinstance(z,torch.cuda.FloatTensor))  #False
+z = z.cuda()  #搬到GPU上
+print(isinstance(z,torch.cuda.FloatTensor))  #
+
+k = 1.  #python
+torch.tensor(k)  #pytorch
+
+p = torch.tensor(2.2)
+print(p.shape)  #torch.Size([])
+len(p.shape)  #0
+print(p.size())  #torch.Size([])
+
+print(torch.tensor([1.1]))
+print(torch.tensor([1.1,2.2]))
+print(torch.FloatTensor(1))  #数据初始化 ，参数：向量长度 tensor([0.])
+data = np.ones(2)  #[1. 1.]
+print(data.shape)  #(2,)
+print(data.size()+"+++++")
+print(torch.from_numpy(data))  #tensor([1., 1.], dtype=torch.float64)
